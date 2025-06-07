3c07745da524c0d6c6c197f73fcdff8f7a473709
nikhilMain <nikhilkumar12130305@gmail.com>
2025-06-08 04:19:25 +0530

    Add files via upload

---
+++ Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+            Navebar</h1>
+    </div>
+  )
+}
+
+export default Navbar
